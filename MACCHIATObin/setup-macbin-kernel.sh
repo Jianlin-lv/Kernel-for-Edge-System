318d822ea64d82c9e840af1d128459ba9883ed32
Jianlin Lv <Jianlin.Lv@arm.com>
2019-07-11 19:31:29 +0800

    Update setup-macbin-kernel.sh
    Signed-off-by: Jianlin Lv <Jianlin.Lv@arm.com>

--- MACCHIATObin/setup-macbin-kernel.sh
+++ MACCHIATObin/setup-macbin-kernel.sh
@@ -13,45 +13,44 @@ export ARCH=arm64
 # Parameter Overridable Paths
 export KDIR=${ROOTDIR}/kernel/4.14.22
 export MUSDK_PATH=${ROOTDIR}/musdk
-export DECONFIG_MCBIN=${ROOTDIR}/defconfig-mcbin-edge
+export DEFCONFIG_MCBIN=${ROOTDIR}/defconfig-mcbin-edge
 
 echo -e "Please run shell script as root!"
 
 # Check file defconfig-mcbin-edge
-if [ ! -f "$DECONFIG_MCBIN" ]; then
-	echo -e "\tPlease copy defconfig-mcbin-edge to currently directory!"
-	exit 1
+if [ ! -f "$DEFCONFIG_MCBIN" ]; then
+    echo -e "\tPlease copy defconfig-mcbin-edge to currently directory!"
+    exit 1
 fi
 
 
 # Download Kernel Source
 echo -e "Download marvell linux 18.09..."
 mkdir -p $KDIR
-cd $KDIR
-#touch kernle-test
+cd $KDIR || exit
+#touch kernel-test
 git clone https://github.com/MarvellEmbeddedProcessors/linux-marvell .
 git checkout linux-4.14.22-armada-18.09
-cd $ROOTDIR
+cd $ROOTDIR || exit
 
 # Download MUSDK Package
 echo -e "Download MUSDK package 18.09..."
 mkdir -p $MUSDK_PATH
-cd $MUSDK_PATH
+cd $MUSDK_PATH || exit
 #touch musdk-test
 git clone https://github.com/MarvellEmbeddedProcessors/musdk-marvell .
 git checkout musdk-armada-18.09
-cd $ROOTDIR
 
 #Patch kernel
-cd $KDIR
+cd $KDIR || exit
 echo -e "Patch kernel..."
 #touch patch_kernel
 git am $MUSDK_PATH/patches/linux-4.14/*.patch
 
 # Check file defconfig-mcbin-edge
-if [ ! -f "$DECONFIG_MCBIN" ]; then
-	echo -e "\tPlease copy defconfig-mcbin-edge to $ROOTDIR!"
-	exit 1
+if [ ! -f "$DEFCONFIG_MCBIN" ]; then
+    echo -e "\tPlease copy defconfig-mcbin-edge to $ROOTDIR!"
+    exit 1
 fi
 
 
@@ -59,7 +58,7 @@ fi
 echo -e "Backup mvebu_v8_lsp_defconfig"
 mv $KDIR/arch/arm64/configs/mvebu_v8_lsp_defconfig $KDIR/arch/arm64/configs/mvebu_v8_lsp_defconfig.bac
 echo -e "Replease kernel config by defconfig-mcbin-edge"
-cp $DECONFIG_MCBIN $KDIR/arch/arm64/configs/mvebu_v8_lsp_defconfig
+cp $DEFCONFIG_MCBIN $KDIR/arch/arm64/configs/mvebu_v8_lsp_defconfig
 echo -e "Build Kernel..."
 make mvebu_v8_lsp_defconfig
 make -j$(($(nproc)+1))
@@ -72,4 +71,3 @@ cp ./arch/arm64/boot/dts/marvell/armada-8040-mcbin.dtb  /boot/
 sync
 
 echo -e "Success! Please reboot!"
-
